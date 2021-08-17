885f32467c4ef626af5420c4e9b30e469e7d2f50
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-17 10:31:55 +0900

    ⚙️ :: Init Project

---
+++ src/main/java/com/example/noticeboardv2/NoticeBoardV2Application.java
@@ -0,0 +1,13 @@
+package com.example.noticeboardv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeBoardV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noticeboardv2/NoticeBoardV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noticeboardv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
