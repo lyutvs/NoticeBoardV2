2de3ee6169d4ef94e109b2db8168b2afd5998213
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-17 22:33:24 +0900

    ⚡️ :: PK 필드 명시

--- src/main/java/com/example/noticeboardv2/domain/posts/Posts.java
+++ src/main/java/com/example/noticeboardv2/domain/posts/Posts.java
@@ -1,2 +1,43 @@
-package com.example.noticeboardv2.domain.posts;public class Posts {
+package com.example.noticeboardv2.domain.posts;
+
+import com.example.noticeboardv2.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@ToString
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 450, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    private Long authorId;
+
+    @Builder
+    public Posts(String title, String content, String author, Long authorId) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.authorId = authorId;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
