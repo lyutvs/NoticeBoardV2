e1ccbd562325fc24bbf487d32fb25a04abc6ae47
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-17 22:39:27 +0900

    ⚡️ :: 사용자 권한 룰

--- src/main/java/com/example/noticeboardv2/domain/user/Role.java
+++ src/main/java/com/example/noticeboardv2/domain/user/Role.java
@@ -1,2 +1,16 @@
-package com.example.noticeboardv2.domain.user;public enum Role {
+package com.example.noticeboardv2.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자"),
+    ADMIN("ROLE_ADMIN", "운영자");
+
+    private final String key;
+    private final String title;
 }
