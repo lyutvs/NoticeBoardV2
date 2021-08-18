154551723bcf0d277e2d50b4132cf440dba9fec9
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-18 18:55:23 +0900

    ⚡️ :: PostsService

--- src/main/java/com/example/noticeboardv2/service/PostsService.java
+++ src/main/java/com/example/noticeboardv2/service/PostsService.java
@@ -1,10 +1,20 @@
 package com.example.noticeboardv2.service;
 
+import com.example.noticeboardv2.domain.posts.Posts;
 import com.example.noticeboardv2.domain.posts.PostsRepository;
+import com.example.noticeboardv2.dto.web.PostsListResponseDTO;
+import com.example.noticeboardv2.dto.web.PostsResponseDTO;
 import com.example.noticeboardv2.dto.web.PostsSaveRequestDTO;
+import com.example.noticeboardv2.dto.web.PostsUpdateRequestDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -17,4 +27,60 @@ public class PostsService {
         return postsRepository.save(requestDTO.toEntity())
                 .getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDTO requestDTO) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+        posts.update(requestDTO.getTitle(), requestDTO.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDTO findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+
+        return new PostsResponseDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDTO> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDTO::new).collect(Collectors.toList());
+    }
+
+    // 페이지로 가져오기
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDTO> findAllByOrderByIdDesc(Integer pageNum, Integer postsPerPage) {
+        Page<Posts> page = postsRepository.findAll(
+                // PageRequest의 page는 0부터 시작
+                PageRequest.of(pageNum - 1, postsPerPage,
+                        Sort.by(Sort.Direction.DESC, "id")
+                ));
+        return page.stream()
+                .map(PostsListResponseDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public Long count() {
+        return postsRepository.count();
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts post = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+
+        postsRepository.delete(post);
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDTO> searchTitleAndContent(String keyword) {
+        return postsRepository
+                .findByContentContainingIgnoreCaseOrTitleContainingIgnoreCase(keyword, keyword)
+                .stream()
+                .map(PostsListResponseDTO::new)
+                .collect(Collectors.toList());
+    }
 }
+
