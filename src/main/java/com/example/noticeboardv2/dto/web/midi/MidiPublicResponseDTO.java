f9ea4def42449b19491e1fe3bc1e02b1b7a1fa7f
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-18 22:50:17 +0900

    ⚡️ :: MidiPublicResponseDTO

--- src/main/java/com/example/noticeboardv2/dto/web/midi/MidiPublicResponseDTO.java
+++ src/main/java/com/example/noticeboardv2/dto/web/midi/MidiPublicResponseDTO.java
@@ -1,2 +1,39 @@
-package com.example.noticeboardv2.dto.web.midi;public class MidiPublicResponseDTO {
-}
+package com.example.noticeboardv2.dto.web.midi;
+
+import com.example.noticeboardv2.domain.midi.Midi;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.apache.tomcat.jni.Local;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@NoArgsConstructor
+public class MidiPublicResponseDTO {
+
+    private Long id;
+
+    private Long userId;    // 업로드한 사람
+
+    private String category;
+
+    private String customTitle;
+
+    private String originalFileName;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime modifiedDate;
+
+    public MidiPublicResponseDTO(Midi midi) {
+        this.id = midi.getId();
+        this.userId = midi.getUserId();
+        this.category = midi.getCategory();
+        this.customTitle = midi.getCustomTitle();
+        this.originalFileName = midi.getOriginalFileName();
+        this.createdDate = midi.getCreatedDate();
+        this.modifiedDate = midi.getModifiedDate();
+    }}
