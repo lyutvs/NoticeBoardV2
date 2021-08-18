02dc1536c6d4068f9c14a2dea2bc138b1a464165
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-18 22:49:41 +0900

    ⚡️ :: MidiRepository

--- src/main/java/com/example/noticeboardv2/domain/midi/MidiRepository.java
+++ src/main/java/com/example/noticeboardv2/domain/midi/MidiRepository.java
@@ -1,2 +1,12 @@
-package com.example.noticeboardv2.domain.midi;public interface MidiRepository {
+package com.example.noticeboardv2.domain.midi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MidiRepository extends JpaRepository<Midi, Long> {
+
+    Midi findByHash(String hash);
+
+    List<Midi> findByUserId(Long userId);
 }
