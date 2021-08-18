c60d6474c9fee0bce7e84dd0bf9a5a1aa056107f
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-18 18:55:15 +0900

    ⚡️ :: PostsUpdateRequestDTO

--- src/main/java/com/example/noticeboardv2/dto/web/PostsUpdateRequestDTO.java
+++ src/main/java/com/example/noticeboardv2/dto/web/PostsUpdateRequestDTO.java
@@ -1,2 +1,20 @@
-package com.example.noticeboardv2.dto.web;public class PostsUpdateRequestDTO {
+package com.example.noticeboardv2.dto.web;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDTO {
+
+    private String title, content;
+
+    @Builder
+    public PostsUpdateRequestDTO(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
 }
+
