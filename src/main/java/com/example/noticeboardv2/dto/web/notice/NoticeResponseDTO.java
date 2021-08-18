96716a9ebd19c1f0d34f7ce8607b4b6d1e02c836
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-18 22:51:06 +0900

    ⚡️ :: NoticeResponseDTO

--- src/main/java/com/example/noticeboardv2/dto/web/notice/NoticeResponseDTO.java
+++ src/main/java/com/example/noticeboardv2/dto/web/notice/NoticeResponseDTO.java
@@ -1,2 +1,23 @@
-package com.example.noticeboardv2.dto.web.notice;public class NoticeResponseDTO {
+package com.example.noticeboardv2.dto.web.notice;
+
+import com.example.noticeboardv2.domain.notice.Notice;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class NoticeResponseDTO {
+
+    private Long id, authorId;
+    private String title, content, author;
+
+    @Setter
+    private Long viewCount;
+
+    public NoticeResponseDTO(Notice entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+        this.authorId = entity.getAuthorId();
+    }
 }
