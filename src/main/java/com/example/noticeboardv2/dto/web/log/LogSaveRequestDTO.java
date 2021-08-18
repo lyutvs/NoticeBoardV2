85ad68556297abbee35a382a3ffa84daad770a78
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-18 18:54:38 +0900

    ⚡️ :: LogSaveRequestDTO

--- src/main/java/com/example/noticeboardv2/dto/web/log/LogSaveRequestDTO.java
+++ src/main/java/com/example/noticeboardv2/dto/web/log/LogSaveRequestDTO.java
@@ -1,2 +1,32 @@
-package com.example.noticeboardv2.dto.web.log;public class LogSaveRequestDTO {
+package com.example.noticeboardv2.dto.web.log;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.java.Log;
+
+@Getter
+@NoArgsConstructor
+public class LogSaveRequestDTO {
+
+    private String boardName, ipAddress;
+    private Long articleId, userId;
+
+    @Builder
+    public LogSaveRequestDTO(String boardName, Long articleId, Long userId, String ipAddress) {
+        this.boardName = boardName;
+        this.articleId = articleId;
+        this.userId = userId;
+        this.ipAddress = ipAddress;
+    }
+
+    public Log toEntity() {
+        return Log.builder()
+                .boardName(boardName)
+                .articleId(articleId)
+                .userId(userId)
+                .ipAddress(ipAddress)
+                .build();
+    }
+
 }
