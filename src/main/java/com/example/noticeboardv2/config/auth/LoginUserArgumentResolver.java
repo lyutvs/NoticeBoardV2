ebf34d1f51cebc53f8e9b9055333f6d3b6d9aa34
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-19 22:48:55 +0900

    ⚡️ :: LoginUserArgumentResolver

--- src/main/java/com/example/noticeboardv2/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/example/noticeboardv2/config/auth/LoginUserArgumentResolver.java
@@ -1,2 +1,44 @@
-package com.example.noticeboardv2.config.auth;public class LoginUserArgumentResolver {
+package com.example.noticeboardv2.config.auth;
+
+import com.example.noticeboardv2.annotation.LoginUser;
+import com.example.noticeboardv2.dto.auth.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+
+        // 파라미터에 @LoginUser 어노테이션이 붙어있는지?
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+
+        // 파라미터의 클래스 타입이 dto.SessionUser.class 인지?
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isUserClass;
+
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+
+        // 파라미터에 전달할 객체 생성
+        return httpSession.getAttribute("user");
+    }
+
 }
