a1f461334e5c8dda3a82ff5c96da7d785442b568
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-19 22:51:42 +0900

    ⚡️ :: BasicResponsDTO

--- src/main/java/com/example/noticeboardv2/dto/web/BasicResponseDTO.java
+++ src/main/java/com/example/noticeboardv2/dto/web/BasicResponseDTO.java
@@ -1,2 +1,12 @@
-package com.example.noticeboardv2.dto.web;public class BasicResponseDTO {
+package com.example.noticeboardv2.dto.web;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class BasicResponseDTO {
+
+    private final String name;
+    private final int amount;
 }
