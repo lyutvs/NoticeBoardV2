f33419c0474d2033bbec576008736f65190e5ae1
엄청난노옴 <lyutvs@gmail.ocm>
2021-08-19 22:54:10 +0900

    🧪  :: CatTest

--- src/test/java/com/example/noticeboardv2/domain/web/dto/toy/CarTest.java
+++ src/test/java/com/example/noticeboardv2/domain/web/dto/toy/CarTest.java
@@ -1,2 +1,26 @@
-package com.example.noticeboardv2.domain.web.dto.toy;public class CarTest {
+package com.example.noticeboardv2.domain.web.dto.toy;
+
+import com.example.noticeboardv2.toy.Car;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @Test
+    public void 빌더_테스트_1() {
+        String id = "1";
+        String name = "Sehyoun";
+
+        // Car car1 = new Car(id, name)
+        // Car car2 = new Car(name, car) // ??
+//        Car car1 = Car.builder()
+//                .id(id)
+//                .name(name)
+//                .build();
+
+//        assertThat(car1.getId()).isEqualTo(id);
+//        assertThat(car1.getName()).isEqualTo(name);
+
+    }
 }
